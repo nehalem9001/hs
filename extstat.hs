a06748007999832665b9efe65cee8bce395a9cda
nehalem <1453constantinopole@gmail.com>
2014-11-11 18:42:44 +0100

    more improvements (files list, count)...

--- extstat.hs
+++ extstat.hs
@@ -1,10 +1,12 @@
 --Haskell--
 import System.Console.GetOpt
+import Data.List
 import Control.Applicative
 import Control.Monad
 import System.Environment
 import System.Directory
 import Control.Monad
+import System.FilePath.Posix
 
 helpText :: String
 helpText = unlines 
@@ -45,29 +47,31 @@ defaultOptions = Options { optVersion	= False
 
 options :: [OptDescr (Options -> IO Options)]
 options = 
-		[ Option 	"h" 	["help"]	   		(NoArg (\opts -> return opts { optHelp = True} )) 			"Help"
-		, Option 	"a" 	["all"]    			(NoArg (\opts -> return opts { optVersion = True} )) 		"Hidden files"
-		, Option 	"v" 	["version"] 		(NoArg (\opts -> return opts { optVersion = True} )) 		"Version"
-		, Option 	"e"	 	["extension"] 		(NoArg (\opts -> return opts { optVersion = True} )) 		"Sort by extensions"
-		, Option 	"c" 	["count"] 			(NoArg (\opts -> return opts { optVersion = True} )) 		"Sort by count of files"
-		, Option 	"t" 	["total"]			(NoArg (\opts -> return opts { optVersion = True} )) 		"Show total number of files"
+		[ Option 	"h" 	["help"]	   		(NoArg (\opts -> return opts { optHelp = True} )) 		"Help"
+		, Option 	"a" 	["all"]    			(NoArg (\opts -> return opts { optAll = True} )) 		"Include hidden files"
+		, Option 	"v" 	["version"] 		(NoArg (\opts -> return opts { optVersion = True} ))	"Version"
+		, Option 	"e"	 	["extension"] 		(NoArg (\opts -> return opts { optExt = True} )) 		"Sort by extensions"
+		, Option 	"c" 	["count"] 			(NoArg (\opts -> return opts { optCount = True} )) 		"Sort by count of files"
+		, Option 	"t" 	["total"]			(NoArg (\opts -> return opts { optTotal = True} )) 		"Show total number of files"
+		, Option 	"t" 	["human"]			(NoArg (\opts -> return opts { optHuman = True} )) 		"Show sizes in human readable form"
 		]
 
-getDirectories :: [FilePath] -> IO [FilePath]
-getDirectories [] = return []
-getDirectories fplist = liftM2 (++) files $ dirs >>= return.filter ((/= '.').head) >>= getDirectories
+getDirectories :: [FilePath] -> (String -> Bool) -> IO [FilePath]
+getDirectories [] _ = return []
+getDirectories fplist flt = liftM2 (++) files $ dirs >>= return.filter (\x -> x /= "." && x /= ".." && (flt x)) >>= flip getDirectories flt
 							where
-								dirs 	= filterDirectoryContent fp doesDirectoryExist
-								files 	= filterDirectoryContent fp doesFileExist
+								dirs 	= filterDirectoryContent fp flt doesDirectoryExist
+								files 	= filterDirectoryContent fp flt doesFileExist
 								fp 		= map (\p -> if last p /= '/' then p ++ "/" else p) fplist
 
 
-filterDirectoryContent :: [FilePath] -> (FilePath -> IO Bool) -> IO [FilePath]
-filterDirectoryContent [] _ = return []
-filterDirectoryContent fp g = filterM doesDirectoryExist fp
-								>>= mapM (\x -> getDirectoryContents x >>= return.filter ((/= '.').head) >>= return.map (x++))
+filterDirectoryContent :: [FilePath] -> (String -> Bool) -> (FilePath -> IO Bool) -> IO [FilePath]
+filterDirectoryContent [] flt _ = return []
+filterDirectoryContent fp flt g = filterM doesDirectoryExist fp
+								>>= mapM (\x -> getDirectoryContents x >>= return.filter (\x -> x /= "." && x /= ".." && (flt x)) >>= return.map (x++))
 								>>= return.concat
 								>>= filterM g
+
 main = do
 	args <- getArgs
 	let (actions, nonOpts, errors) = getOpt Permute options args
@@ -81,6 +85,10 @@ main = do
 				, optAll = all } = opts	
 	when help printHelp
 	when version printVersion
-	let dirs = last args	
-	paths <- getDirectories [dirs]
-	sequence_ $ map putStrLn paths
+	let includeHidden = if all then (\_ -> True) else (\x -> head x /= '.')
+	let dirs = last args
+	paths <- getDirectories [dirs] includeHidden
+	fileStatuses <- mapM getFileStatus paths
+	let extensions = groupBy (\p1 p2 -> snd p1 == snd p2).sortBy (\u v -> compare (snd u) (snd v)).filter (not.null.snd) $ map splitExtension paths
+	let sizes = map (\p -> (snd (head p), length p)) extensions
+	mapM (\p -> putStrLn ((show (fst p)) ++ " " ++ show (snd p))) (sortBy (\u v -> compare (snd u) (snd v)) sizes)
